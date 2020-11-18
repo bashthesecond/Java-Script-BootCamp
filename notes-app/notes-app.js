094959570f95f3a077ad1a2acb2ff5fbfc3b58de
Olawale Bashiru <66824020+bashthesecond@users.noreply.github.com>
2020-11-18 22:28:01 +0100

    Add files via upload

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,14 +1,20 @@
-//DOM - Document Object Module
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to spain'
+}, {
+    title: 'Habits to improve',
+    body: 'Living healthy. Being conscious of having a clean environment. my communication.'
+}, {
+    title: 'Pending projects',
+    body: 'Javascript - in view. React Native - yet to start'
+}]
 
-//Query and remove
-// const p = document.querySelector('p')
-// p.remove()
+document.querySelector('#create-note').addEventListener('click', function(e) {
+    e.target.textContent = "Button Clicked"
+})
 
-//Query all and remove
-const ps = document.querySelectorAll('p')
-
-ps.forEach(function (p) {
-    p.textContent = '********'
-    // console.log(p.textContent)
-    // p.remove()
+document.querySelector('#remove-notes').addEventListener('click', function() {
+    document.querySelectorAll('.note').forEach(function(note) {
+        note.remove()
+    })
 })
