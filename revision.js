94bbf44ce2a29be41aafeffc51f782e740b6b06a
Olawale Bashiru <66824020+bashthesecond@users.noreply.github.com>
2020-12-01 07:50:03 +0100

    Add files via upload

--- revision.js
+++ revision.js
@@ -1,28 +1,24 @@
-//Global
-    //Local
-        //Local
-    //Local
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
 
+let otherBook = {
+    title: 'A peoples History',
+    author: 'Howard Zinn',
+    pageCount: 723
+}
 
-let temp = 170
-
-//60 & 90 (nice out there)
-// if (temp >= 60 && temp <= 90) {
-//     console.log('It is pretty nice out there')
-//  }
-
-// if (temp >= 120 || temp <= 0) {
-//     console.log('Do not go outside!')
-// } 
-
-let name = 'Olawale'
-
-if (true) {
-    if (true) {
-        console.log(name)
+let getSummary = function (book) {
+    return {
+        summary: `${book.title} by ${book.author}`,
+        pageCountSummary: `${book.title} is ${book.pageCount} pages long`
     }
 }
 
-if (true) {
+let bookSummary = getSummary(myBook)
+let otherBookSummary = getSummary(otherBook)
 
-}
+console.log(bookSummary.pageCountSummary)
+console.log(otherBookSummary.summary) 
