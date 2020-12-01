7517c8f69f5d84d1006ac57908a327193bd4f9f7
Olawale Bashiru <66824020+bashthesecond@users.noreply.github.com>
2020-12-01 07:58:19 +0100

    Add files via upload

--- revision.js
+++ revision.js
@@ -1,24 +1,7 @@
-let myBook = {
-    title: '1984',
-    author: 'George Orwell',
-    pageCount: 326
+if (true) {
+    var firstName = 'Olawale' 
 }
 
-let otherBook = {
-    title: 'A peoples History',
-    author: 'Howard Zinn',
-    pageCount: 723
-}
+firstName =  'Qudus'
 
-let getSummary = function (book) {
-    return {
-        summary: `${book.title} by ${book.author}`,
-        pageCountSummary: `${book.title} is ${book.pageCount} pages long`
-    }
-}
-
-let bookSummary = getSummary(myBook)
-let otherBookSummary = getSummary(otherBook)
-
-console.log(bookSummary.pageCountSummary)
-console.log(otherBookSummary.summary) 
+console.log(firstName)
