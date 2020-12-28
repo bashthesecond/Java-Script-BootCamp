6018c9583e88c54fe144d59f8f68eb5e37f3ed30
Olawale Bashiru <66824020+bashthesecond@users.noreply.github.com>
2020-12-28 08:00:16 +0100

    Add files via upload

---
+++ hangman/scripts/requests.js
@@ -0,0 +1,37 @@
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {})
+    
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.puzzle
+    } else {
+        throw new Error('Unable to fetch puzzle')
+    }    
+   
+}
+
+const getCountry = async (countryCode) => {
+    const response = await fetch('http://restcountries.eu/rest/v2/all')
+    if (response.status === 200) {
+        const countryList = await response.json()
+        return countryList.find((country) => country.alpha2Code === countryCode)
+    } else {
+        throw new Error('Unable to fetch data')
+    }
+}
+
+
+const getLocation = async () => {
+    const response = await fetch('https://ipinfo.io/json?htttp://ipinfo.io/json?token=c27ec015602b05')
+    if (response.status === 200) {
+        return response.json()
+    }else {
+        throw new Error('Unable to fetch location')
+    }
+    
+}
+    
+const getCurrentCountry = async () => {
+    const location = await getLocation()
+    return getCountry(location.country)
+}
---
+++ notes-app/scripts/notes-edit.js
@@ -0,0 +1,60 @@
+'use strict'
+
+//Get id of note from url
+const titleElement = document.querySelector('#note-title')
+const bodyElement =  document.querySelector('#note-body')
+const lastEditedElement = document.querySelector('#last-edited')
+const noteID = location.hash.substring(1)
+let notes = getSavedNotes()
+let note = notes.find((note) => note.id === noteID)
+
+if (!note) {
+    location.assign('/index.html')
+}
+
+//setup lest edited
+
+
+//setup note title  
+titleElement.value = note.title
+bodyElement.value = note.body
+lastEditedElement.textContent = updateLastEdited(note.updatedAt)
+
+titleElement.addEventListener('input', (e) => {
+    note.title = e.target.value
+    note.updatedAt = getTimestamp()
+    lastEditedElement.textContent = updateLastEdited(note.updatedAt)
+    saveNotes(notes)
+})
+
+//setup note body
+bodyElement.addEventListener('input', (e) => {
+    note.body = e.target.value
+    note.updatedAt = getTimestamp()
+    lastEditedElement.textContent = updateLastEdited(note.updatedAt)
+    saveNotes(notes)
+})
+
+//setup remove button
+noteIndex = notes.findIndex((note) => note.id === noteID)
+document.querySelector('#remove-note').addEventListener('click', (e) => {
+    removeNote(noteID)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
+
+// Make window listen for updates in local storage to universally update editors
+window.addEventListener('storage', (e) => {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        note = notes.find((note) => note.id === noteID)
+        
+        if (!note) {
+            location.assign('/index.html')
+        }
+        titleElement.value = note.title
+        bodyElement.value = note.body
+        lastEditedElement.textContent = updateLastEdited(note.updatedAt)
+    }
+})
+
