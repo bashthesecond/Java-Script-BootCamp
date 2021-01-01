dc280a59b3182d07ec15f29c19429092dd4e64e8
bashthesecond <qbashy@gmail.com>
2021-01-01 08:25:08 +0100

    Update

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,10 +1,1 @@
-const name = 'Mike'
-console.log(name)
-
-class Hangman {
-    myMethod () {
-        return 'testing'
-    }
-}
-const hangman = new Hangman
-console.log(hangman.myMethod())
+console.log('index.js')
