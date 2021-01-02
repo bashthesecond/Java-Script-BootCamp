fdd533ceb1a1b7bbe20e1b758647cc00ad711628
bashthesecond <qbashy@gmail.com>
2021-01-02 06:08:38 +0100

    update

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,1 +1,6 @@
+import {add} from './utilities'
+import {scream} from './scream.js'
+
 console.log('index.js')
+console.log(add(20, 5))
+console.log(scream('olawale'))
--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -1,1 +1,3 @@
 console.log('utilities.js')
+
+export const add = (a, b) => a + b
