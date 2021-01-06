ba04bb638b27c4117ca7d4ed9ef1ae9f840c12fc
bashthesecond <qbashy@gmail.com>
2021-01-06 08:09:01 +0100

    update

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,7 +1,1 @@
-import square, { add, name } from './utilities'
-import scream from './scream.js'
-
-console.log('index.js')
-console.log(add(65, 5))
-console.log(scream(name))
-console.log(square(4))
+console.log('index.js') 
