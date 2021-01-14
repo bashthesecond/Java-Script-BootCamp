5e2ae58c4cf6736422303e18ca54ac011630afb9
bashthesecond <qbashy@gmail.com>
2021-01-14 20:47:01 +0100

    update

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,1 +1,21 @@
-console.log('index.js')
+import { getNotes, createNote, removeNote, updateNote } from './notes'
+import { getFilters, setFilters } from './filters'
+
+// console.log(getNotes())
+// createNote()
+// removeNote()
+
+// updateNote('8e2bf9f6-077c-4303-9510-dd2e1d0d3e48', {
+//     title: 'My note title',
+//     body: 'My note body'
+// })
+// console.log(getNotes())
+
+console.log(getFilters())
+
+setFilters({
+    searchText: 'office',
+    sortBy: 'byCreated'
+})
+
+console.log(getFilters())
