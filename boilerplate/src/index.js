0ce78ae7f121bb2f4b375b96d467fade3a6fa5ce
bashthesecond <qbashy@gmail.com>
2021-01-17 12:27:10 +0100

    updates

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -5,3 +5,34 @@ const printTeam = (teamName, coach, ...players) => {
 }
 
 printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry')
+
+let house = {
+    bedrooms: 2,
+    bathrooms: 1.5,
+    yearBuilt: 2017
+}
+
+let newHouse = {
+    yearBuilt: 2020,
+    ...house
+}
+
+console.log(house)
+console.log(newHouse)
+
+const person = {
+    name: 'Olawale',
+    age: 26
+}
+
+const location = {
+    city: 'Ogun',
+    country: 'Nigeria'
+}
+
+const overview = {
+    ...person,
+    ...location
+}
+
+console.log(overview)
