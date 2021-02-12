f916f959a0b4420b1f7ef983a92e266bb4075c79
bashthesecond <qbashy@gmail.com>
2021-02-12 08:12:26 +0100

    update

--- portfolio/src/functions.js
+++ portfolio/src/functions.js
@@ -1,12 +1,17 @@
-const panImage = function (e) {
+const panImage = (e) => {
     this.style.backgroundPositionX = -e.offsetX + "px"
     this.style.backgroundPositionY = -e.offsetY + "px"
-    console.log(event)
 }
 
-const mouseAnime = function(e, panElement) {
+const mouseAnime = (e, panElement) => {
     panElement.style.left = e.pageX + 'px'
     panElement.style.top = e.pageY + 'px'
 }
 
-export { panImage, mouseAnime }
+const setTheme = (script, theme) => {
+    script.href = `./styles/theme/${theme}.css`
+    localStorage.setItem('style', theme)
+}
+
+
+export { panImage, mouseAnime, setTheme }
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,12 +1,12 @@
-import { panImage, mouseAnime } from './functions'
+import { panImage, mouseAnime, setTheme } from './functions'
 import jstz from 'jstz'
 import moment from 'moment'
 import tz from 'moment-timezone'
 
-const panElement = document.querySelector('.container')
-document.addEventListener('mousemove', (e) => {
-    mouseAnime(e, panElement)
-})
+// const panElement = document.querySelector('.container')
+// document.addEventListener('mousemove', (e) => {
+//     mouseAnime(e, panElement)
+// })
 
 //Time Zone
 if (!sessionStorage.getItem('timezone')) {
@@ -17,9 +17,26 @@ const currentTimezone = sessionStorage.getItem('timezone')
 const now = moment()
 console.log(moment(now).tz(currentTimezone).format('DD MM YYYY hh:mm:ss'))
 
-//Theme Switcher
-let switches = document.querySelectorAll('switch')
+// Theme Switcher
+const switcher = document.querySelector('#switcher-script')
+const themeSwitch = document.querySelector('.switch')
 let style = localStorage.getItem('style')
 
-style = null ? setTheme('light') : setTheme(style)
+document.addEventListener("DOMContentLoaded", (e) => {
+    !style ? setTheme(switcher, 'light') : setTheme(switcher, style)
+})
+
+themeSwitch.addEventListener('click', (e) => {
+    style = localStorage.getItem('style')
+    if (style === 'light') {
+        console.log('clicked')
+        setTheme(switcher, 'dark')
+        themeSwitch.style.backgroundPositionX = 'right'
+    } else if (style === 'dark') {
+        console.log('clicked')
+        setTheme(switcher, 'light')
+        themeSwitch.style.backgroundPositionX = 'left'
+    }
+})
+
 
