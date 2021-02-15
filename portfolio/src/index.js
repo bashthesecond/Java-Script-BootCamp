802ced12a2c513a8c138dca860f2d6196749d641
bashthesecond <qbashy@gmail.com>
2021-02-15 04:31:26 +0100

    update

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,42 +1,42 @@
-import { panImage, mouseAnime, setTheme } from './functions'
-import jstz from 'jstz'
-import moment from 'moment'
-import tz from 'moment-timezone'
+// import { panImage, mouseAnime, setTheme } from './functions'
+// import jstz from 'jstz'
+// import moment from 'moment'
+// import tz from 'moment-timezone'
 
-// const panElement = document.querySelector('.container')
-// document.addEventListener('mousemove', (e) => {
-//     mouseAnime(e, panElement)
+// // const panElement = document.querySelector('.container')
+// // document.addEventListener('mousemove', (e) => {
+// //     mouseAnime(e, panElement)
+// // })
+
+// //Time Zone
+// if (!sessionStorage.getItem('timezone')) {
+//     const timezone = jstz.determine()
+//     sessionStorage.setItem('timezone', timezone.name())
+// }
+// const currentTimezone = sessionStorage.getItem('timezone')
+// const now = moment()
+// console.log(moment(now).tz(currentTimezone).format('DD MM YYYY hh:mm:ss'))
+
+// // Theme Switcher
+// const switcher = document.querySelector('#switcher-script')
+// const themeSwitch = document.querySelector('.switch')
+// let style = localStorage.getItem('style')
+
+// document.addEventListener("DOMContentLoaded", (e) => {
+//     !style ? setTheme(switcher, 'light') : setTheme(switcher, style)
 // })
 
-//Time Zone
-if (!sessionStorage.getItem('timezone')) {
-    const timezone = jstz.determine()
-    sessionStorage.setItem('timezone', timezone.name())
-}
-const currentTimezone = sessionStorage.getItem('timezone')
-const now = moment()
-console.log(moment(now).tz(currentTimezone).format('DD MM YYYY hh:mm:ss'))
-
-// Theme Switcher
-const switcher = document.querySelector('#switcher-script')
-const themeSwitch = document.querySelector('.switch')
-let style = localStorage.getItem('style')
-
-document.addEventListener("DOMContentLoaded", (e) => {
-    !style ? setTheme(switcher, 'light') : setTheme(switcher, style)
-})
-
-themeSwitch.addEventListener('click', (e) => {
-    style = localStorage.getItem('style')
-    if (style === 'light') {
-        console.log('clicked')
-        setTheme(switcher, 'dark')
-        themeSwitch.style.backgroundPositionX = 'right'
-    } else if (style === 'dark') {
-        console.log('clicked')
-        setTheme(switcher, 'light')
-        themeSwitch.style.backgroundPositionX = 'left'
-    }
-})
+// themeSwitch.addEventListener('click', (e) => {
+//     style = localStorage.getItem('style')
+//     if (style === 'light') {
+//         console.log('clicked')
+//         setTheme(switcher, 'dark')
+//         themeSwitch.style.backgroundPositionX = 'right'
+//     } else if (style === 'dark') {
+//         console.log('clicked')
+//         setTheme(switcher, 'light')
+//         themeSwitch.style.backgroundPositionX = 'left'
+//     }
+// })
 
 
