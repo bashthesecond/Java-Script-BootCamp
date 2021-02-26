976f087d16aace04f2dba183add0d723ef3c9c08
bashthesecond <qbashy@gmail.com>
2021-02-26 18:22:15 +0100

    update

--- holycake/src/functions.js
+++
@@ -1,7 +0,0 @@
-const panImage = function (e) {
-    this.style.backgroundPositionX = -e.offsetX + "px"
-    this.style.backgroundPositionY = -e.offsetY + "px"
-    console.log(event)
-}
-
-export { panImage as default }
--- holycake/src/index.js
+++
@@ -1,9 +0,0 @@
-import panImage from './functions.js'
-
-const toggleControl = document.querySelector('.nav-toggle')
-const mobileNav = document.querySelector('.mobile-nav')
-
-toggleControl.addEventListener('click', (e) => {
-    mobileNav.classList.toggle('active')
-})
-
